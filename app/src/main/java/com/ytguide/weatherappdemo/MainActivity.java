5f8712c652ba647480d400af4fdfaa8c22871de9
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-18 10:57:58 +0100

    Completed GET request

--- app/src/main/java/com/ytguide/weatherappdemo/MainActivity.java
+++ app/src/main/java/com/ytguide/weatherappdemo/MainActivity.java
@@ -9,6 +9,18 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     Button btn_cityID;
     Button btn_getWeatherByID;
@@ -34,7 +46,55 @@ public class MainActivity extends AppCompatActivity {
         btn_cityID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "You clicked me 1", Toast.LENGTH_SHORT).show();
+                // ...
+
+// Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://www.metaweather.com/api/location/search/?query=" + et_dataInput.getText().toString() ;
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url,null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String cityID = "";
+                        try {
+                            JSONObject cityInfo = response.getJSONObject(0);
+                             cityID = cityInfo.getString("woeid");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        Toast.makeText(MainActivity.this, "City ID = " + cityID, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error occured ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                queue.add(request);
+
+// Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                               Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(MainActivity.this, "Error occured ", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+// Add the request to the RequestQueue.
+
+
+
+
+                //Toast.makeText(MainActivity.this, "You clicked me 1", Toast.LENGTH_SHORT).show();
             }
         });
 
