9750165d89642c5b1e5821ec86cd71ebda48f5aa
LukeBraverman <l.braverman.l@live.co.uk>
2021-05-18 10:13:50 +0100

    Finished button listeners

--- app/src/main/java/com/ytguide/weatherappdemo/MainActivity.java
+++ app/src/main/java/com/ytguide/weatherappdemo/MainActivity.java
@@ -3,12 +3,57 @@ package com.ytguide.weatherappdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_cityID;
+    Button btn_getWeatherByID;
+    Button btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // assign values from layouts to java files
+        btn_cityID = findViewById(R.id.btn_getCityId);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityId);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+
+
+        //click liseners
+
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked me 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked me 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
